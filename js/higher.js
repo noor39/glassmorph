4c7a87cf6e2b7cb6118e4282e5d7ebb6604b819c
Kazi Noor Ahmed <44676977+noor39@users.noreply.github.com>
2024-10-29 16:32:20 +0600

    Add files via upload

---
+++ js/higher.js
@@ -0,0 +1,10 @@
+// let total=0,  count =1;
+// while(count <=10){
+//     total+=count;
+//     count+=1;}
+// console.log(total);
+let total=0,  count =1;
+while(count <=10){
+    total+=count;
+    count+=1;}
+console.log(count);
